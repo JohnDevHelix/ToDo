8fea073067079904476811625f2ce57ac8b45447
JohnDevHelix <johndevproject@gmail.com>
2025-02-04 17:23:29 +0800

    Added index

--- src/newTaskClass.js
+++ src/newTaskClass.js
@@ -1,9 +1,10 @@
 export class NewTask {
-  constructor(title, description, dueDate, bgColor, label) {
+  constructor(title, description, dueDate, bgColor, label, index) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.bgColor = bgColor;
     this.label = label;
+    this.index = index;
   }
 }
