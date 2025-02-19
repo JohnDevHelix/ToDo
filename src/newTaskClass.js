2397ee9e968cd57df79f1689c84f26ef85e14f38
JohnDevHelix <johndevproject@gmail.com>
2025-02-19 16:29:27 +0800

    minor changes

--- src/newTaskClass.js
+++ src/newTaskClass.js
@@ -1,9 +1,8 @@
 export class NewTask {
-  constructor(title, description, dueDate, bgColor, label, index) {
+  constructor(title, description, dueDate, label, index) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
-    this.bgColor = bgColor;
     this.label = label;
     this.index = index;
   }
